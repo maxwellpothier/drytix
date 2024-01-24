051717e1d765bd8c8c513f2f1f8b92331eaeb78e
hisaacweaver <isaac.weaver99@gmail.com>
2024-01-24 16:12:34 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 	return (
 		<div className="flex justify-center">
 			<h1 className="text-center text-4xl" style={{marginTop: "100px"}}>
-				DryTix
+				DryTix by BYU Students
 			</h1>
 		</div>
 	);
