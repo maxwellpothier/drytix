288ce5301e2deb55dcad6bd8d4867469173d3646
maxwellpothier <pothiermaxwell@gmail.com>
2024-01-23 17:19:04 -0700

    switching repos

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className="flex justify-center">
+			<h1 className="text-center text-4xl" style={{marginTop: "100px"}}>
+				DryTix is coming soon!
+			</h1>
+		</div>
+	);
+}
