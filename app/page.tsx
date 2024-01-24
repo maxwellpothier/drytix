96ca59124c66236b5db769847c0def8ec5e08ddc
maxwellpothier <pothiermaxwell@gmail.com>
2024-01-23 17:21:48 -0700

    test vercel link (again)

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 	return (
 		<div className="flex justify-center">
 			<h1 className="text-center text-4xl" style={{marginTop: "100px"}}>
-				DryTix is coming soon!
+				DryTix
 			</h1>
 		</div>
 	);
