46217541e9b173636c280a76e7c910bb44acb5a4
maxwellpothier <pothiermaxwell@gmail.com>
2024-01-29 18:03:53 -0700

    unused import

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import {ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Image from "next/image";
 import logo from "../public/img/Dry.png";
-import clothes from "../public/img/dry-clean.jpeg";
+// import clothes from "../public/img/dry-clean.jpeg";
 
 type FormValues = {
 	name: string;
